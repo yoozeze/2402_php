266389be9b70671f045532a848eb96a34ad886ad
yoozeze <fbghrud3@naver.com>
2024-03-20 21:53:03 +0900

    20240320 ~ homework

--- PHP/tng/0320_homework.php
+++ PHP/tng/0320_homework.php
@@ -1,9 +1,26 @@
 <?php
+// while 문
+echo "while 문\n";
 $num = 1;
-$num3 = 0;
-while($num < 101){
-    $num++;
-    $num3 = ($num % 3 === 0);
-    echo $num."입니다.\n";
+while ($num < 101){
+    echo "$num 입니다. ";
+    $num += 2;
+}
+
+// foreach
+echo "\nforeach 문\n";
+$arr_num = range(1, 100);
+foreach($arr_num as $val){
+    if($val % 3 !== 0){
+        echo "$val 입니다. ";
     }
 }
+
+// for 문
+echo "\nfor 문\n";
+for($i = 1; $i < 101; $i++){
+    if($i % 3 !== 0){
+        echo "$i 입니다. ";
+    }
+}
+
