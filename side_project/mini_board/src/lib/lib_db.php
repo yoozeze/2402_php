bb035c770b6a08cfb5712ba695d95b68e146589f
yoozeze <fbghru3@naver.com>
2024-03-25 18:08:18 +0900

    20240325 ~ list.php

---
+++ side_project/mini_board/src/lib/lib_db.php
@@ -0,0 +1,58 @@
+<?php
+
+function my_db_conn(){
+    // 설정 정보
+    $option = [
+        PDO::ATTR_EMULATE_PREPARES      => FALSE
+        ,PDO::ATTR_ERRMODE              => PDO::ERRMODE_EXCEPTION
+        ,PDO::ATTR_DEFAULT_FETCH_MODE   => PDO::FETCH_ASSOC
+    ];
+
+    // 리턴
+    return new PDO(MARIADB_DSN, MARIADB_USER, MARIADB_PASSWORD, $option);
+}
+
+function db_select_boards_cnt(&$conn){
+    // sql 작성
+    $sql =
+    " SELECT "	
+	."  COUNT(no) as cnt "
+    ." FROM "
+	."  boards "
+    ." WHERE "
+	."  deleted_at IS NULL "	
+    ;
+
+    // Query 실행
+    $stmt = $conn->query($sql);
+    $result = $stmt->fetchAll();
+
+    // 리턴
+    return (int)$result[0]["cnt"];
+}
+
+
+function db_select_boards_paging(&$conn, &$array_param){
+    // sql 작성
+    $sql =
+        " SELECT "	
+        ."  no "
+        ."  ,title "
+        ."  ,created_at "
+        ." FROM "
+        ."  boards "
+        ." WHERE "
+        ."  deleted_at IS NULL "
+        ." ORDER BY "	
+        ."  no DESC "
+        ." LIMIT :list_cnt OFFSET :offset "	
+    ;
+
+    // Query 실행
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($array_param);
+    $result = $stmt->fetchAll();
+
+    // 리턴
+    return $result;
+}
