8eaf6f3dc446ab5ffb5e2faeb60e3f2183f62f26
yoozeze <fbghrud3@naver.com>
2024-04-30 22:21:57 +0900

    20240430 side

--- side_project/연습용/todo_doc/sql/todolistdatabase.sql
+++ side_project/연습용/todo_doc/sql/todolistdatabase.sql
@@ -1,27 +1,28 @@
-CREATE DATABASE todo_list;
-USE todo_list;
+CREATE DATABASE todolist;
+USE todolist;
 CREATE TABLE todolist(
 	NO  				INT 				PRIMARY KEY AUTO_INCREMENT
 	,today 			DATE				NOT NULL
 	,day_goals 		VARCHAR(100) 	NOT NULL
-	,todo			VARCHAR(100)	NOT NULL 	
+	,todo				VARCHAR(100)	NOT NULL
+	,checked_com	CHAR(1)			NOT NULL
 	,created_at 	DATE				NOT NULL DEFAULT CURRENT_DATE()
 	,updated_at 	DATE				NOT NULL DEFAULT CURRENT_DATE()
 	,deleted_at 	DATE 
 );
 
-INSERT INTO todolist (today, day_goals, todo)
+INSERT INTO todolist (today, day_goals, todo, checked_com)
 VALUES
-('2024-03-01', '목표', '할일')
-,('2024-03-02', '목표', '할일')
-,('2024-03-03', '목표', '할일')
-,('2024-03-04', '목표', '할일')
-,('2024-03-05', '목표', '할일')
-,('2024-03-06', '목표', '할일')
-,('2024-03-07', '목표', '할일')
-,('2024-03-08', '목표', '할일')
-,('2024-03-09', '목표', '할일')
-,('2024-03-10', '목표', '할일')
+('2024-03-01', '목표', '할일', '1')
+,('2024-03-02', '목표', '할일', '1')
+,('2024-03-03', '목표', '할일', '1')
+,('2024-03-04', '목표', '할일', '0')
+,('2024-03-05', '목표', '할일', '1')
+,('2024-03-06', '목표', '할일', '1')
+,('2024-03-07', '목표', '할일', '0')
+,('2024-03-08', '목표', '할일', '1')
+,('2024-03-09', '목표', '할일', '0')
+,('2024-03-10', '목표', '할일', '0')
 ;
 
 
